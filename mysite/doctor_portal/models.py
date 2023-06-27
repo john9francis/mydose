39aecceea17aae0e082a05e248620772d5b797cb
John Francis <john9francis@gmail.com>
2023-06-27 17:10:43 -0400

    migrated doctor_portal models into the database

--- mysite/doctor_portal/models.py
+++ mysite/doctor_portal/models.py
@@ -7,17 +7,21 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=200)
     birthdate = models.DateTimeField("Birthdate")
 
+
+class Diagnosis(models.Model):
+    '''Different diagnoses or types of cancer'''
+    description = models.CharField(max_length=100)
+
+
 class Plan(models.Model):
     '''A patient's treatment plan, including several treatments.'''
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    diagnosis = models.CharField()
+    diagnosis = models.ForeignKey(Diagnosis, on_delete=models.CASCADE)
     number_of_treatments = models.SmallIntegerField("Treatments")
     total_dose = models.SmallIntegerField("Total dose")
+
 
 class Treatment(models.Model):
     '''includes one radiation treatment on a patient.'''
     pass
 
-class Diagnosis(models.Model):
-    '''Different diagnoses or types of cancer'''
-    pass
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'doctor_portal.apps.DoctorPortalConfig',
+    'patient_portal.apps.PatientPortalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
